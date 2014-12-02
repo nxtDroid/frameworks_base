ef8f8b3c758835825184cb3bbfd41f97b01aa5b9
louis_chang <louis_chang@htc.com>
2014-12-02 12:38:05 +0800

    [ActivityManager] Invalidate last screenshot when activity relaunched
    Symptom:
    The task thumbnail is not updated when activity relaunched.
    
    Reproduce Steps:
    1. Put device in portrait
    2. Launch Calculator
    3. Launch Recent App (the Calculator's screenshot is correct)
    4. Rotate device to landscape
    5. Click Calculator in Recent App to return to Calculator (Calculator has relaunched to landsacpe ui)
    6. Launch Recent App again (the Calculator's screenshot is not updated)
    
    Change-Id: I92e951ea2ee215c52ca6e50cf6f9e02deb787bce

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -1792,6 +1792,9 @@ final class ActivityStack {
                     // Do over!
                     mStackSupervisor.scheduleResumeTopActivities();
                 }
+                if (next == mLastScreenshotActivity) {
+                    invalidateLastScreenshot();
+                }
                 if (mStackSupervisor.reportResumedActivityLocked(next)) {
                     mNoAnimActivities.clear();
                     if (DEBUG_STACK) mStackSupervisor.validateTopActivitiesLocked();
