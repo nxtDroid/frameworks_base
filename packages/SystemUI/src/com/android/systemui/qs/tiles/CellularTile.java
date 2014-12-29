b43a3b4626af0beccf33baf0b450b0d078a2d158
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-29 07:08:46 +0000

    SystemUI: Display data type in QS when Wi-Fi is connected to captive portal
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
    
    Change-Id: Ia5cc25649074a641a3b1dcd62edbbabdba870da0

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -104,8 +104,10 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
         state.icon = ResourceIcon.get(iconId);
         state.isOverlayIconWide = cb.isDataTypeIconWide;
         state.autoMirrorDrawable = !cb.noSim;
-        state.overlayIconId = cb.enabled && (cb.dataTypeIconId > 0) ? cb.dataTypeIconId : 0;
-        state.filter = iconId != R.drawable.ic_qs_no_sim;
+        state.overlayIconId = cb.enabled && (cb.dataTypeIconId > 0)
+                ? cb.dataTypeIconId
+                : 0;
+        state.filter = state.iconId != R.drawable.ic_qs_no_sim;
         state.activityIn = cb.enabled && cb.activityIn;
         state.activityOut = cb.enabled && cb.activityOut;
 
