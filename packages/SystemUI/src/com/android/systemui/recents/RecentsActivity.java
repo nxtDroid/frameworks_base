f6a3f7ee6cd5f2ff6b03a72de8eca38aa5d567b0
snak3ater <saeedyasir91@gmail.com>
2015-01-06 22:31:58 +0500

    Remove PA Clear Recent Apps [1/2]
    Revert "Clear all recents drawable by Rush25"
    
    This reverts commit 0dddc8105859e84c4effc4c94b59b0d48cec5181.
    
    Revert "Framework: Add enabler for Navbar Recents"
    
    This reverts commit 53db763c3a393f51da1bb3ddea5818da31ff7b75.
    
    Revert "clearallrecents: remove dead code"
    
    This reverts commit 2632bffc10c34377070af4397cb44e4d134de449.
    
    Revert "Use 'dismissAllTasks' to clear all apps"
    
    This reverts commit 188dc34d499e2aea92bbe260a76fd063c98fd8cb.
    
    Revert "Don't remove the last app"
    
    This reverts commit 61fa7c990af7792cdd70b91e5d8c0a430cc61fe2.
    
    Revert "Clear Recent Apps (From NavBar)"
    
    This reverts commit b1af5a9a9122d52a70190cf4c98329e2082d8397.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
    
    Change-Id: I247075ce1f9e654772e6055abcb87986c92c2e89

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -19,13 +19,11 @@ package com.android.systemui.recents;
 import android.app.Activity;
 import android.app.ActivityOptions;
 import android.app.SearchManager;
-import android.app.StatusBarManager;
 import android.appwidget.AppWidgetHostView;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
@@ -33,7 +31,6 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.Pair;
 import android.view.KeyEvent;
 import android.view.View;
@@ -58,7 +55,6 @@ import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.SystemUIApplication;
 import com.android.systemui.recents.RecentsConfiguration;
 import android.provider.Settings;
-import com.android.systemui.statusbar.phone.NavigationBarView;
 import java.lang.ref.WeakReference;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -71,7 +67,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
         DebugOverlayView.DebugOverlayViewCallbacks {
 
     RecentsConfiguration mConfig;
-    static boolean mVisible;
+    boolean mVisible;
     long mLastTabKeyEventTime;
 
     // Top level views
@@ -189,29 +185,12 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
         }
     });
 
-    public void setRecentHints(boolean show) {
-        // Check if we need to enable alternate drawable for recent apps key
-        if(mNavigationCallback == null) return; // Multiuser is not allowed
-        int navigationHints = mNavigationCallback.getNavigationIconHints();
-        mNavigationCallback.setNavigationIconHints(NavigationBarView.NAVBAR_RECENTS_HINT,
-        show ? (navigationHints | StatusBarManager.NAVIGATION_HINT_RECENT_ALT)
-        : (navigationHints & ~StatusBarManager.NAVIGATION_HINT_RECENT_ALT), true);
-    }
-
-    public static void setNavigationBarView(NavigationBarView nav) {
-        mNavigationBarView = nav;
-    }
-
-    public static void setNavigationCallback(NavigationCallback callback) {
-        mNavigationCallback = callback;
-    }
-
     /** Updates the set of recent tasks */
     void updateRecentsTasks(Intent launchIntent) {
         // If AlternateRecentsComponent has preloaded a load plan, then use that to prevent
         // reconstructing the task stack
         RecentsTaskLoader loader = RecentsTaskLoader.getInstance();
-        root = loader.reload(this,
+        SpaceNode root = loader.reload(this,
                 Constants.Values.RecentsTaskLoader.PreloadFirstTasksCount,
                 mConfig.launchedFromHome);
         ArrayList<TaskStack> stacks = root.getStacks();
@@ -294,39 +273,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
 
         // Animate the SystemUI scrims into view
         mScrimViews.prepareEnterRecentsAnimation();
-
-        if (launchIntent.getAction().equals(AlternateRecentsComponent.ACTION_CLEAR_RECENTS_ACTIVITY)) {
-            if (mVisible && mRecentsView != null) {
-                mRecentsView.clearRecents();
-            }
-        }
-    }
-
-    public static boolean hasTaskStacks() {
-        return root.hasTasks();
-    }
-
-    public static boolean isActivityShowing() {
-        return mVisible;
-    }
-
-    /** Check if Clear All Recents is enabled */
-    public boolean clearAllRecentsNavbarEnabled() {
-        return Settings.System.getInt(getContentResolver(),
-                Settings.System.CLEAR_ALL_RECENTS_NAVBAR_ENABLED, 1) != 0;
-    }
-
-    private void updateNavigationBarIcon() {
-        if (mVisible) {
-            if (root != null && clearAllRecentsNavbarEnabled()) {
-                setRecentHints(root.hasTasks());
-            } else {
-                setRecentHints(false);
-            }
-        } else {
-            // It's nothing
-            setRecentHints(false);
-        }
     }
 
     /** Attempts to allocate and bind the search bar app widget */
@@ -522,15 +468,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
 
         // Mark Recents as visible
         mVisible = true;
-
-        updateNavigationBarIcon();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        updateNavigationBarIcon();
     }
 
     @Override
